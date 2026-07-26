0f569932c48f882a1b3a3272cd2d26e76c77bc1a
tanish3106 <choudharytanish2805@gmail.com>
2026-07-26 14:21:49 +0530

    Time: 4 ms (4.07%) | Memory: 8.4 MB (22.83%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int nextNumber(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+
+        while (n != 1 && seen.find(n) == seen.end()) {
+            seen.insert(n);
+            n = nextNumber(n);
+        }
+
+        return n == 1;
+    }
+};
