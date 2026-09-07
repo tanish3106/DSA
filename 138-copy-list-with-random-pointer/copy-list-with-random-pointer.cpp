00f6e6448641c36b6221f3fce2aac07888944ca0
tanish3106 <choudharytanish2805@gmail.com>
2026-09-07 13:44:37 +0530

    Time: 7 ms (51.65%) | Memory: 15 MB (87.43%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+
+        if (head == nullptr)
+            return nullptr;
+
+        
+
+        Node* curr = head;
+
+        while (curr != nullptr) {
+            Node* copy = new Node(curr->val);
+
+            copy->next = curr->next;
+            curr->next = copy;
+
+            curr = copy->next;
+        }
+
+
+    
+
+        curr = head;
+
+        while (curr != nullptr) {
+
+            if (curr->random != nullptr) {
+                curr->next->random = curr->random->next;
+            }
+
+            curr = curr->next->next;
+        }
+
+
+        
+
+        curr = head;
+
+        Node* copyHead = head->next;
+
+        while (curr != nullptr) {
+
+            Node* copy = curr->next;
+
+            
+            curr->next = copy->next;
+
+            
+            if (copy->next != nullptr) {
+                copy->next = copy->next->next;
+            }
+
+            curr = curr->next;
+        }
+
+        return copyHead;
+    }
+};
